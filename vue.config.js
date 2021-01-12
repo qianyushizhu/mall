84adf75ffca5d08ff48ade4f5e12e4a62b40e2b7
qianyushizhu <73497435+qianyushizhu@users.noreply.github.com>
2021-01-12 18:09:31 +0800

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    //发布模式
+    chainWebpack: config => {
+        config.when(process.env.NODE_ENV === 'production', config => {
+            config.entry('app').clear().add('./src/main-prod.js')
+        })
+        //开发模式
+          config.when(process.env.NODE_ENV === 'development', config => {
+            config.entry('app').clear().add('./src/main-dev.js')
+        })
+    }
+}
